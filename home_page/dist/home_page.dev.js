3ec7fab012104fe5326c023e2556437e82cfe592
EgrWvyLeo <74408233+EgrWvyLeo@users.noreply.github.com>
2021-01-13 23:46:01 +0000

    V4

--- home_page/dist/home_page.dev.js
+++ home_page/dist/home_page.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // menu interaction
 var sn = document.querySelector("#side_nav");
 var closeMenu = document.querySelector("#close_side_nav");
@@ -6,50 +8,49 @@ var menu = document.querySelector("#top_menu");
 menu.onmousedown = function open_side_nav() {
   sn.style.display = "block";
 };
+
 closeMenu.onclick = function close_side_nav() {
   sn.style.display = "none";
-};
+}; // style
 
-// style
+
 var centerBox_all = document.querySelectorAll(".centerBox");
-
 var Style = document.querySelector("#Style");
 var style = document.querySelector("#style");
 var style_close = document.querySelector("#style_close");
 
 Style.onclick = function () {
-  for (let i = 0; i < centerBox_all.length; i++) {
+  for (var i = 0; i < centerBox_all.length; i++) {
     centerBox_all[i].style.display = "none";
   }
+
   style.style.display = "block";
 };
 
 style_close.onclick = function () {
   style.style.display = "none";
-};
+}; // credits
 
-// credits
+
 var Credits = document.querySelector("#Credits");
 var credits = document.querySelector("#credits");
 var credits_close = document.querySelector("#credits_close");
 
 Credits.onclick = function () {
-  for (let i = 0; i < centerBox_all.length; i++) {
+  for (var i = 0; i < centerBox_all.length; i++) {
     centerBox_all[i].style.display = "none";
   }
+
   credits.style.display = "block";
 };
+
 credits_close.onclick = function () {
   credits.style.display = "none";
-};
-
-// var centerBox_close = document.querySelector(".centerBox_close");
+}; // var centerBox_close = document.querySelector(".centerBox_close");
 // var centerBox = document.querySelector(".centerBox");
-
 // centerBox_close.onclick = function () {
 //   centerBox.style.display = "none";
 // };
-
 // if ((credits.style.display = "none")) {
 //   Credits.onclick = function () {
 //     credits.style.display = "block";
@@ -59,7 +60,6 @@ credits_close.onclick = function () {
 //     credits.style.display = "none";
 //   };
 // }
-
 // var bool = false;
 // if ((bool = false)) {
 //   Credits.onclick = function () {
@@ -72,8 +72,9 @@ credits_close.onclick = function () {
 //     credit.style.display = "none";
 //   };
 // }
-
 // landmarks pin interaction
+
+
 var pin_1 = document.querySelector("#pin_1");
 var pin_2 = document.querySelector("#pin_2");
 var pin_3 = document.querySelector("#pin_3");
@@ -94,12 +95,11 @@ test_close.onclick = function () {
 };
 
 pin_1.onclick = function () {
-  landmark_image.style.backgroundImage =
-    "url('../image/landmarks/Jerónimos_April_2009-4.jpg')";
+  landmark_image.style.backgroundImage = "url('../image/landmarks/Jerónimos_April_2009-4.jpg')";
   landmark_image.style.backgroundPosition = "-300px -100px";
   lisbon.style.display = "none";
-  landmark_name.innerHTML = "Mosteiro dos Jerónimos";
-  // landmark_image.style.backgroundSize = "800px";
+  landmark_name.innerHTML = "Mosteiro dos Jerónimos"; // landmark_image.style.backgroundSize = "800px";
+
   landmark_test.onclick = function () {
     question.innerHTML = "What stands between Jeronimos and the river?";
     A.innerHTML = "A: A parking lot";
@@ -111,42 +111,34 @@ pin_1.onclick = function () {
 };
 
 pin_2.onclick = function () {
-  landmark_image.style.backgroundImage =
-    "url('../image/landmarks/photo-1562620287-9309c2d9a460-816x612.jpg";
+  landmark_image.style.backgroundImage = "url('../image/landmarks/photo-1562620287-9309c2d9a460-816x612.jpg";
   landmark_image.style.backgroundPosition = "-200px -130px";
   lisbon.style.display = "none";
-  landmark_name.innerHTML = "Torre de Belém";
-  // landmark_image.style.backgroundSize = "900px";
+  landmark_name.innerHTML = "Torre de Belém"; // landmark_image.style.backgroundSize = "900px";
+
   landmark_test.onclick = function () {
     question.innerHTML = "What was this landmarks' originally built for?";
     A.innerHTML = "A: It was originally intended to be a defensive stronghold	";
     B.innerHTML = "B: A prison to lisbon's most notorious criminals	";
-    C.innerHTML =
-      "C: Point of embarkation and disembarkment for portuguese explorers	";
-    D.innerHTML = "D: One of the king's private places";
-    // test_pos.style.width = "1000px";
+    C.innerHTML = "C: Point of embarkation and disembarkment for portuguese explorers	";
+    D.innerHTML = "D: One of the king's private places"; // test_pos.style.width = "1000px";
+
     test_pos.style.display = "block";
   };
 };
 
 pin_3.onclick = function () {
-  landmark_image.style.backgroundImage =
-    "url('../image/landmarks/engenhos-da-calheta-madeira-2.jpg";
+  landmark_image.style.backgroundImage = "url('../image/landmarks/engenhos-da-calheta-madeira-2.jpg";
   landmark_image.style.backgroundPosition = "-100px -130px";
   lisbon.style.display = "none";
-  landmark_name.innerHTML = "Castelo São Jorge";
-  // landmark_image.style.backgroundSize = "cover";
+  landmark_name.innerHTML = "Castelo São Jorge"; // landmark_image.style.backgroundSize = "cover";
+
   landmark_test.onclick = function () {
-    question.innerHTML =
-      "Why is castelo de são jorge located on that specific location?";
-    A.innerHTML =
-      "A: Closer to the sky makes the towers warmer for the soldiers";
-    B.innerHTML =
-      "B: His hill is the tallest closest to the river enabling height advantage and quick sea acess";
-    C.innerHTML =
-      "C: There is not a real reason for the location of the castle";
-    D.innerHTML =
-      "D: Lisbon being located in na area with 7 hills the 4rth one was chosen for superstitious reasons";
+    question.innerHTML = "Why is castelo de são jorge located on that specific location?";
+    A.innerHTML = "A: Closer to the sky makes the towers warmer for the soldiers";
+    B.innerHTML = "B: His hill is the tallest closest to the river enabling height advantage and quick sea acess";
+    C.innerHTML = "C: There is not a real reason for the location of the castle";
+    D.innerHTML = "D: Lisbon being located in na area with 7 hills the 4rth one was chosen for superstitious reasons";
     test_pos.style.display = "block";
   };
 };
