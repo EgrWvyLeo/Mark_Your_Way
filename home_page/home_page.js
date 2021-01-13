52f0181a33a54753954d4d6bda15ca5715ddca1c
EgrWvyLeo <74408233+EgrWvyLeo@users.noreply.github.com>
2021-01-13 14:23:23 +0000

    V3

--- home_page/home_page.js
+++ home_page/home_page.js
@@ -1,3 +1,4 @@
+// menu interaction
 var sn = document.querySelector("#side_nav");
 var closeMenu = document.querySelector("#close_side_nav");
 var menu = document.querySelector("#top_menu");
@@ -5,7 +6,69 @@ var menu = document.querySelector("#top_menu");
 menu.onmousedown = function open_side_nav() {
   sn.style.display = "block";
 };
-
 closeMenu.onclick = function close_side_nav() {
   sn.style.display = "none";
 };
+
+// style
+var centerBox_all = document.querySelectorAll(".centerBox");
+
+var Style = document.querySelector("#Style");
+var style = document.querySelector("#style");
+var style_close = document.querySelector("#style_close");
+
+Style.onclick = function () {
+  for (let i = 0; i < centerBox_all.length; i++) {
+    centerBox_all[i].style.display = "none";
+  }
+  style.style.display = "block";
+};
+
+style_close.onclick = function () {
+  style.style.display = "none";
+};
+
+// credits
+var Credits = document.querySelector("#Credits");
+var credits = document.querySelector("#credits");
+var credits_close = document.querySelector("#credits_close");
+
+Credits.onclick = function () {
+  for (let i = 0; i < centerBox_all.length; i++) {
+    centerBox_all[i].style.display = "none";
+  }
+  credits.style.display = "block";
+};
+credits_close.onclick = function () {
+  credits.style.display = "none";
+};
+
+// var centerBox_close = document.querySelector(".centerBox_close");
+// var centerBox = document.querySelector(".centerBox");
+
+// centerBox_close.onclick = function () {
+//   centerBox.style.display = "none";
+// };
+
+// if ((credits.style.display = "none")) {
+//   Credits.onclick = function () {
+//     credits.style.display = "block";
+//   };
+// } else if ((credits.style.display = "block")) {
+//   Credits.onclick = function () {
+//     credits.style.display = "none";
+//   };
+// }
+
+// var bool = false;
+// if ((bool = false)) {
+//   Credits.onclick = function () {
+//     credit.style.display = "block";
+//     bool = true;
+//   };
+// }
+// if ((bool = true)) {
+//   Credits.onclick = function () {
+//     credit.style.display = "none";
+//   };
+// }
